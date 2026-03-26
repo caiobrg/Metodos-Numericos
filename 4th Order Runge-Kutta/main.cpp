4ecfd3bbd90caede599f9e1381b83b75747e2748
caio <caio@cabmj.com>
2026-03-26 00:58:27 -0300

    Adicionado solver para RK4

---
+++ 4th Order Runge-Kutta/main.cpp
@@ -0,0 +1,55 @@
+#include "solver_rk4.hpp"
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+int main() {
+  // Create the results directory (if it doesn't already exists)
+  std::filesystem::create_directory("results/St");
+  std::filesystem::create_directory("results/Dt");
+  std::filesystem::create_directory("results/Re");
+
+  double stopTime = 10.0; // amount of "time" to simulate for
+  double inicialVelocity = 0.0;
+
+  std::cout << "Starting simulations..." << std::endl;
+
+  // ---------------------------------------------------------
+  // 1: Re -> 0 for diferent values of Stokes' number
+  // ---------------------------------------------------------
+  double standardDeltaT = 0.01;
+  double stokes_values[] = {0.1, 0.5, 1.0, 1.5, 2.0};
+
+  for (double St : stokes_values) {
+    std::string filename =
+        "results/St/resultados_St_" + std::to_string(St) + ".csv";
+    solver_rk4(St, 0.0, inicialVelocity, stopTime, standardDeltaT, filename);
+  }
+
+  // ---------------------------------------------------------
+  // 2: Influence of the size of deltaT (discretization)
+  // ---------------------------------------------------------
+  double St = 1.0;
+  double Re = 0.5;
+  double deltaT_values[] = {2.0, 0.5, 0.1, 0.05, 0.01};
+
+  for (double dt : deltaT_values) {
+    std::string filename =
+        "results/Dt/resultados_dt_" + std::to_string(dt) + ".csv";
+    solver_rk4(St, Re, inicialVelocity, stopTime, dt, filename);
+  }
+
+  // ---------------------------------------------------------
+  // Analises 3, 4 e 5: Efeito Inercial (Re != 0)
+  // ---------------------------------------------------------
+  double reynolds_values[] = {0, 0.1, 0.5, 1.0};
+
+  for (double Re : reynolds_values) {
+    std::string filename =
+        "results/Re/resultados_Re_" + std::to_string(Re) + ".csv";
+    solver_rk4(St, Re, inicialVelocity, stopTime, standardDeltaT, filename);
+  }
+
+  std::cout << "Simulations completed" << std::endl;
+  return 0;
+}
---
+++ 4th Order Runge-Kutta/solver_rk4.hpp
@@ -0,0 +1,50 @@
+// Libraries
+#pragma once // Evita que o arquivo seja incluído mais de uma vez
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+inline double dimensionless_velocity_derivative(double stokesNumber,
+                                                double reynoldsNumber,
+                                                double velocity) {
+  return ((1.0 - velocity - 3.0 / 8.0 * reynoldsNumber * velocity * velocity) /
+          stokesNumber);
+}
+
+inline int solver_rk4(double stokesNumber, double reynoldsNumber,
+                      double initialVelocity, double stopTime, double deltaT,
+                      const std::string &filename) {
+
+  std::ofstream outputFile(filename);
+
+  outputFile << std::setprecision(15);
+  
+  if (!outputFile.is_open()) {
+    std::cerr << "Erro: Nao foi possivel criar o arquivo." << std::endl;
+    return 0; // Encerra o programa com erro
+  }
+
+  outputFile << "Time(t*),Velocity(v*)\n";
+
+  double velocity = initialVelocity;
+
+  for (double t = 0; t < stopTime; t = t + deltaT) {
+    outputFile << t << "," << velocity << "\n";
+
+    double k1 = dimensionless_velocity_derivative(stokesNumber, reynoldsNumber,
+                                                  velocity);
+    double k2 = dimensionless_velocity_derivative(stokesNumber, reynoldsNumber,
+                                                  velocity + (k1 * deltaT / 2));
+    double k3 = dimensionless_velocity_derivative(stokesNumber, reynoldsNumber,
+                                                  velocity + (k2 * deltaT / 2));
+    double k4 = dimensionless_velocity_derivative(stokesNumber, reynoldsNumber,
+                                                  velocity + (k3 * deltaT));
+
+    velocity = velocity + (deltaT / 6) * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
+  }
+
+  outputFile.close();
+
+  return 0;
+}
